345329c3a4b3eb4346716eb662002726ad2ebace
Tekiter <tekiter8@gmail.com>
2020-03-16 01:49:04 +0900

    Test lint-staged

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
+        <p>hello world</p>
         <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
 </template>
