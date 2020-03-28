c532bbf6dc4647d4b444eb048a3a114d78317f28
Tekiter <tekiter8@gmail.com>
2020-03-29 03:56:12 +0900

    Add result page

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-        <mini-schedule></mini-schedule>
-    </div>
+    <v-container fluid>
+        <v-row>
+            <v-col>
+                <mini-schedule></mini-schedule>
+            </v-col>
+            <v-col>
+                hjk
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
+import { mapState } from 'vuex'
+
 import MiniSchedule from '../components/result/MiniSchedule'
 export default {
     components: {
         MiniSchedule,
     },
+    computed: {
+        ...mapState('group', ['groups']),
+    },
+    created() {},
 }
 </script>
