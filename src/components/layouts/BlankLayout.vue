61905a57080cd1479c4f0679abd91fbaccddb67b
Tekiter <tekiter8@gmail.com>
2021-05-15 18:44:50 +0900

    Restart Everything

--- src/components/layouts/BlankLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <v-content>
-        <div>
-            <v-fade-transition hide-on-leave>
-                <router-view></router-view>
-            </v-fade-transition>
-        </div>
-    </v-content>
-</template>
